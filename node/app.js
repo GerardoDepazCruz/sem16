7cb6aa00cb662f4e02853f416e95aedb549a833a
André <andre@gmail.com>
2025-11-27 11:26:01 -0500

    wee

--- node/app.js
+++ node/app.js
@@ -1,1 +1,12 @@
-console.log("¡Hola desde un contenedor Docker ejecutando Node.js!");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+res.send("Hola desde Node.js dentro de un contenedor Docker");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+console.log("Servidor ejecutándose en puerto" + PORT);
+});
